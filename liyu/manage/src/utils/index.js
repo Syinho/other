59aa1a63c668b033d09c0b460661d7d45d8490ee
donglei <1220320280@qq.com>
2023-05-10 00:14:07 +0800

    xxx

--- liyu/manage/src/utils/index.js
+++ liyu/manage/src/utils/index.js
@@ -36,26 +36,54 @@ export const handleTime = function (timeStamp) {
     }
 }
 
-export const translate = {
-    bmi: '身体质量指数',
-    pulmonary: '肺活量',
-    jump: '跳远',
+const translateTable = {
+    height: '身高',
+    weight: '体重',
+    bmi_score: '身高/体重得分',
+    curlorup_score: '仰卧起坐',
+    end_score: '最终得分',
     flexion: '坐位体前屈',
+    flexion_score: '坐位体前屈得分',
+    jump: '跳远',
+    jump_score: '跳远得分',
+    left_eye: '左眼视力',
+    pull_up: '引体向上',
+    pulmonary: '肺活量',
+    pulmonary_score: '肺活量得分',
+    right_eye: '右眼视力',
     run50: '50m',
     run800: '800m',
     run1000: '1000m',
-    adbominal_curl: '仰卧起坐',
-    pull_up: '引体向上',
-    runlong:'800米/1000米',
-    curlorup:'引体向上/仰卧起坐',
-    end:'最终'
+    run50_score: '50m得分',
+    runlong_score: '800m/1000m得分',
 }
-export const translateObj=[
-    {name:'身高',value:'height'},{name:'体重',value:'weight'},{name:''}
+export const translate=function(key){
+    return translateTable[key]
+} 
+export const translateObj = [
+    { name: '身高', value: 'height' },
+    { name: '体重', value: 'weight' },
+    { name: '身高/体重得分', value: 'bmi_score' },
+    { name: '仰卧起坐', value: 'curlorup_score' },
+    { name: '最终得分', value: 'end_score' },
+    { name: '坐位体前屈', value: 'flexion' },
+    { name: '坐位体前屈得分', value: 'flexion_score' },
+    { name: '跳远', value: 'jump' },
+    { name: '跳远得分', value: 'jump_score' },
+    { name: '左眼视力', value: 'left_eye' },
+    { name: '引体向上', value: 'pull_up' },
+    { name: '肺活量', value: 'pulmonary' },
+    { name: '肺活量得分', value: 'pulmonary_score' },
+    { name: '右眼视力', value: 'right_eye' },
+    { name: '50m', value: 'run50' },
+    { name: '800m', value: 'run800' },
+    { name: '1000m', value: 'run1000' },
+    { name: '50m得分', value: 'run50_score' },
+    { name: '800m/1000m得分', value: 'runlong_score' },
 ]
 
-export const handle_time=function(time){
-    let m=Math.floor(Number(time)/60)
-    let s=Number(time)%60
+export const handle_time = function (time) {
+    let m = Math.floor(Number(time) / 60)
+    let s = Number(time) % 60
     return `${m}'${s}`
 }
--- liyu/manage/src/views/ViewTaskData/ViewTaskData.vue
+++ liyu/manage/src/views/ViewTaskData/ViewTaskData.vue
@@ -1,29 +1,118 @@
 <template>
     <h2 :style="{ marginBottom: '20px' }">数据统计</h2>
-    <el-select v-model="value1" multiple placeholder="请选择您要查看的字段" style="width: 240px">
-        <el-option
-            v-for="item in options_fields"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
-        />
-    </el-select>
-    <el-select v-model="value1" multiple placeholder="请选择您要查看的数据类型" style="width: 240px">
-        <el-option
-            v-for="item in options_dataType"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
-        />
-    </el-select>
+    <el-form>
+        <el-form-item label="体测项目选择">
+            <el-select
+                v-model="form.fields"
+                multiple
+                placeholder="请选择您要查看的字段"
+                style="width: 240px"
+                size="small"
+                @change="disable"
+            >
+                <el-option
+                    v-for="item in options_fields"
+                    :key="item.value"
+                    :label="item.name"
+                    :value="item.value"
+                />
+            </el-select>
+        </el-form-item>
+        <el-form-item label="数据类型选择">
+            <el-select
+                v-model="form.data"
+                multiple
+                placeholder="请选择您要查看的数据类型"
+                style="width: 240px"
+                size="small"
+                @change="disable"
+            >
+                <el-option
+                    v-for="item in options_dataType"
+                    :key="item.value"
+                    :label="item.name"
+                    :value="item.value"
+                />
+            </el-select>
+        </el-form-item>
+        <el-form-item>
+            <el-button @click="query" type="primary" size="small">执行查询</el-button>
+        </el-form-item>
+    </el-form>
+    <div>
+        <el-descriptions
+            :title="translate(field)"
+            v-for="(field, index) in fields"
+            :key="'field' + index"
+            column="2"
+            border
+        >
+            <el-descriptions-item :label="item" v-for="(item, index) in data">
+                {{ queryResult[field][item] }}
+            </el-descriptions-item>
+        </el-descriptions>
+    </div>
 </template>
 
 <script setup>
-import {ref,reactive} from 'vue'
-import {reqStatisticalData} from '@/ajax/api.js'
-const options_fields=ref([{name:''}])
-const options_dataType=ref([])
-
+import { ref, reactive, watch } from 'vue'
+import { translateObj, translate } from '@/utils/index.js'
+import { reqStatisticalData } from '@/ajax/api.js'
+import { useRoute, useRouter } from 'vue-router'
+const $router = useRouter()
+const $route = useRoute()
+const task_id = $route.params.id
+const options_fields = ref(translateObj)
+const options_dataType = ref([
+    { name: '最大值', value: 'max' },
+    { name: '最小值', value: 'min' },
+    { name: '良好', value: 'good' },
+    { name: '及格', value: 'median' },
+    { name: '优秀', value: 'excellent' },
+    { name: '及格', value: 'qualified' },
+    { name: '不及格', value: 'unqualified' },
+])
+const translate_ = ref({
+    max: '最大值',
+    min: '最小值',
+    good: '良好',
+    median: '及格',
+    excellent: '优秀',
+    qualified: '及格',
+    unqualified: '不及格',
+})
+const form = reactive({
+    fields: [],
+    data: [],
+})
+watch(
+    () => form.fields,
+    newval => {
+        console.log(newval)
+    }
+)
+const show = ref(false)
+const queryResult = ref({})
+const fields = ref([])
+const data = ref([])
+const query = async function () {
+    const res = await reqStatisticalData({ task_id, items: form.fields, statics_names: form.data })
+    if (res.code === 200) {
+        const data_ = JSON.parse(res.data)
+        queryResult.value = data_
+        fields.value = form.fields
+        data.value = form.data
+        show.value = true
+    } else {
+        ElMessage.error(res.msg ? res.msg : '查询错误')
+    }
+}
+const disable = function () {
+    fields.value = []
+    data.value = []
+}
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import './ViewTaskData.scss';
+</style>
